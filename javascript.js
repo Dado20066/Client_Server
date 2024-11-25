01218b3894624a1a1947cbe274c60c0805a864dc
Your Name <you@example.com>
2024-11-25 12:01:24 +0100

    Modifica finale parte di login e prima modifica di invio messaggio

--- javascript.js
+++ javascript.js
@@ -1,36 +1,45 @@
- type="text/javascript">
+type = "text/javascript" >
   "use strict";
 
 
-  let ws = new WebSocket("ws://10.1.0.52:7070/serverchat/chat24");
+let ws = new WebSocket("ws://10.1.0.52:7070/serverchat/chat24");
 
 
-  ws.onmessage = function(messaggioRicevuto) {
-    document.getElementById("ricezione").textContent += messaggioRicevuto.data + "\n";
-  };
+ws.onmessage = function (messaggioRicevuto) {
+  document.getElementById("ricezione").textContent += messaggioRicevuto.data + "\n";
+};
 
 
-  function inviaLogin() {
-    let username = document.getElementById("utente").value;
-    let password = document.getElementById("password").value;
 
-      let messaggioLogin = "L;" + username + ";" + password + ";" + "L;" + ";2024-10-27T18:02:00";
-      ws.send(messaggioLogin);
+function inviaLogin() {
+  let username = document.getElementById("utente").value;
+  let password = document.getElementById("password").value;
 
-      document.getElementById("loginSection").classList.add("hidden").classList.remove("hidden");;
-      document.getElementById("chatSection")
-    }
-    
-  function inviaMessaggio() {
-    let mittente = document.getElementById("mittente").value;
-    let messaggio = document.getElementById("testo").value;
-    let messaggioDaInviare = "M;N;" + mittente + ";iT-IT;" + messaggio + ";2024-10-27T18:02:00;";
+  
+  let messaggioLogin = "L;" + username + ";" + password + ";" + "L;" + ";2024-10-27T18:02:00";
+  ws.send(messaggioLogin);
+
+  document.getElementById("loginSection").classList.add("hidden");
+  document.getElementById("chatSection").classList.add("visibile");
+
+}
+
+
+function inviaMessaggio() {
+  let messaggio = document.getElementById("testo").value;
+
+  if (ws.readyState === WebSocket.OPEN) {
+    let messaggioDaInviare = `M;N;utente;iT-IT;${messaggio};2024-10-27T18:02:00;`;
     ws.send(messaggioDaInviare);
+  } else {
+    alert("Connessione WebSocket non aperta.");
   }
+}
 
 
-  function chiudiLaConnessione() {
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.close();
-    }
+
+function chiudiLaConnessione() {
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.close();
   }
+}
